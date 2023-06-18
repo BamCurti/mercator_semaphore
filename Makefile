84530ee6caea1a3694ad59a2f428f51236940fb1
ie703331 <79431840+ie703331@users.noreply.github.com>
2023-06-18 10:09:51 -0600

    nuevos cambios

--- Makefile
+++ Makefile
@@ -3,5 +3,5 @@ all: mercator mercator_semaphore
 mercator: mercator.c
 	gcc -o mercator mercator.c -lm
 
-mercator_semaphore: mercator_semaphore.c
-	gcc -o mercator_semaphore mercator_semaphore.c -lm
+mercator_semaphore: mercator_semaphore.c semaphoresarr.h
+	gcc -o mercator_semaphore mercator_semaphore.c -lm -pthread
--- mercator_semaphore.c
+++ mercator_semaphore.c
@@ -1,3 +1,9 @@
+/*
+Para compilar incluir la librería m (matemáticas)
+Ejemplo:
+gcc -o mercator mercator.c -lm
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,66 +11,70 @@
 #include <sys/time.h>
 #include <sys/wait.h>
 #include <sys/shm.h>
-#include <semaphore.h>
+#include "semaphoresarr.h"
+
 
 #define NPROCS 4
 #define SERIES_MEMBER_COUNT 200000
+
 
 double *sums;
 double x = 1.0;
 int *proc_count;
 int *start_all;
-int *completed;
 double *res;
+int semarr;
 
-sem_t mutex;
-sem_t all_started;
+enum {EXMUT};
+enum {SYNC};
+
 
 double get_member(int n, double x) {
     int i;
     double numerator = 1;
 
     for(i = 0; i < n; i++) {
-        numerator *= x;
+        numerator = numerator*x;
     }
+
+    // if (n % 2 == 0) return ( - numerator / n );
+    // return numerator/n;
 
     return (n % 2 == 0) ? 
         -numerator / n :
         numerator / n;
 }
 
+
 void proc(int proc_num) {
     int i;
-
-    sem_wait(&all_started);
+	
+	//while(!(*start_all));
+    semwait(semarr, SYNC);
 
     sums[proc_num] = 0;
 
-    for(i = proc_num; i < SERIES_MEMBER_COUNT; i += NPROCS) {
-        sums[proc_num] += get_member(i + 1, x);
+    for(i=proc_num; i < SERIES_MEMBER_COUNT; i+= NPROCS) {
+        sums[proc_num] += get_member(i+1, x);
     }
 
-    sem_wait(&mutex);
-    (*proc_count)++;
-    sem_post(&mutex);
-
-    // Incrementa el contador de procesos completados
-    sem_wait(&mutex);
-    (*completed)++;
-    sem_post(&mutex);
-
+    //(*proc_count)++;
+    semsignal(semarr, EXMUT);
     exit(0);
 }
+
 
 void master_proc() {
     int i;
     sleep(1);
-    sem_post(&all_started);
+    //*start_all = 1;
+    semsignal(semarr, SYNC);
 
-    while (*completed != NPROCS) {
-        // Espera hasta que todos los procesos esclavos hayan terminado
-        usleep(10000); // Agrega una pequeña pausa para liberar el CPU
-    }
+    //while (*proc_count != NPROCS);// busy wait until all threads are done with computation of partial sums
+    semwait(semarr, EXMUT);
+    semwait(semarr, EXMUT);
+    semwait(semarr, EXMUT);
+    semwait(semarr, EXMUT);
 
     *res = 0;
 
@@ -88,44 +98,37 @@ int main() {
     int shmid;
     void *shmstart;
 
-    size_t size_shmid = NPROCS * sizeof(double) + 4 * sizeof(int);
-
-    shmid = shmget(0x1234, size_shmid, 0666 | IPC_CREAT);
+    shmid = shmget(0x1234, NPROCS * sizeof(double) + 2 * sizeof(int), 0666 | IPC_CREAT);
     shmstart = shmat(shmid, NULL, 0);
     sums = shmstart;
 
-    proc_count = (int *)(sums + NPROCS);
-    start_all = (int *)(proc_count + 1);
-    completed = (int *)(start_all + 1);
-    res = (double *)(completed + 1);
+    proc_count = shmstart + NPROCS * sizeof(double);
+    start_all = shmstart + NPROCS * sizeof(double) + sizeof(int);
+    res = shmstart + NPROCS * sizeof(double) + 2 * sizeof(int);
 
     *proc_count = 0;
     *start_all = 0;
-    *completed = 0;
-
-    sem_init(&mutex, 1, 1);
-    sem_init(&all_started, 1, 0);
 
     gettimeofday(&ts, NULL);
     start_ts = ts.tv_sec; // Tiempo inicial
 
-    for(i = 0; i < NPROCS; i++) {
+    for(i=0; i<NPROCS;i++) {
         p = fork();
-        if(p == 0) {
+        if(p==0) {
             proc(i);
         }
     }
 
     p = fork();
 
-    if(p == 0) {
-        master_proc();
+    if(p==0) {
+    master_proc();
     }
 
     printf("El recuento de ln(1 + x) miembros de la serie de Mercator es %d\n", SERIES_MEMBER_COUNT);
     printf("El valor del argumento x es %f\n", (double)x);
 
-    for(int i = 0; i < NPROCS + 1; i++) {
+    for(int i=0; i<NPROCS + 1; i++) {
         wait(NULL);
     }
 
@@ -138,11 +141,6 @@ int main() {
     printf("El resultado es %10.8f\n", *res);
     printf("Llamando a la función ln(1 + %f) = %10.8f\n", x, log(1+x));
 
-    sem_destroy(&mutex);
-    sem_destroy(&all_started);
-
     shmdt(shmstart);
     shmctl(shmid, IPC_RMID, NULL);
-
-    return 0;
 }
